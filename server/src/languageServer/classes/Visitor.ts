ea2c766acd009ea639f494521863f1d7d5937dd6
Kyle Rich <kyledvrich@gmail.com>
2022-11-16 18:38:20 -0800

    Visitor WIP

---
+++ server/src/languageServer/classes/Visitor.ts
@@ -0,0 +1,20 @@
+export interface Visitor<T = void, U = void> {
+    visitAST(n: object, t: T): U;
+
+    visitAST(n: object, t: T): U;
+    visitVarDecl(n: object, t: T): U;
+    visitRecordDecl(n: object, t: T): U; // "RecordDecl" === Struct Declaration
+    visitFieldDecl(n: object, t: T): U; // "FieldDecl" inside "RecordDecl"
+    visitFunctionDecl(n: object, t: T): U;
+    visitParmVarDecl(n: object, t: T): U; // "ParmVarDecl inside "FunctionDecl"
+    visitCompoundStmt(n: object, t: T): U; // list of statements inside "FunctionDecl" // also used for scoping
+    visitDeclStmt(n: object, t: T): U; // some type of declaration inside "FunctionDecl
+    visitImplicitCastExpr(n: object, t: T): U; //
+    visitDeclRefExpr(n: object, t: T): U; //
+    visitBinaryOperator(n: object, t: T): U; // used for assigning variables?
+    visitMemberExpr(n: object, t: T): U;
+    visitIntegralCast(n: object, t: T): U;
+    visitCharacterLiteral(n: object, t: T): U;
+    visitUnaryOperator(n: object, t: T): U;
+
+}
