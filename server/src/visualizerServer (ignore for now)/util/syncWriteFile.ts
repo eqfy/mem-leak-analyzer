406fba665f4936485b808b35cc545671a8d49bcd
Kyle Rich <kyledvrich@gmail.com>
2022-11-21 21:08:08 -0800

    removing visualizer stuff for now
     (stored in another branch called "visualizerStorage")

--- server/src/visualizerServer (ignore for now)/util/syncWriteFile.ts
+++
@@ -1,8 +0,0 @@
-import { writeFileSync } from 'fs';
-import path from 'path';
-
-export function syncWriteFile(filename: string, data: string): void {
-  writeFileSync(path.join(__dirname, filename), data, {
-    flag: 'w'
-  });
-}
