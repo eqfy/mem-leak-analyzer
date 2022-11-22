f761c5ea98f31339ec9cf57b0a0199278d82dbe9
Kyle Rich <kyledvrich@gmail.com>
2022-11-21 20:16:45 -0800

    function decl change

--- server/src/languageServer/ast/Declarations/FunctionDecl.ts
+++ server/src/languageServer/ast/Declarations/FunctionDecl.ts
@@ -9,7 +9,11 @@ export interface FunctionDecl extends ASTNodeWithType, ASTDecl {
     name: string;
     // always has a list of parameters before the function body
     // if function has an empty body (i.e. "{}"), it will not have the StmtList
-    inner?: (FunctionParamDecl | StmtList)[]; // innner is missing in the case of a declaration with no parameters or body
+    inner?: [FunctionParamDecl] | [FunctionParamDecl, StmtList] | [StmtList];
+    // if inner doesnt exist, then its just a declaration without params
+    // if inner is just params, then its a declaration with params
+    // if there is both params, and a stmt list, then it is a declaration with params and a body
+    // if is it just a stmt list, then it is a declaration with no params, but has a body
 }
 
 export function isFunctionDecl(node: ASTNode): node is FunctionDecl {
